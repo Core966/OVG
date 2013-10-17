465e26302464efad0c287d7028f80244acc1b5f0
Szalay Tibor <core966@debian-webserv.debian-webserv-szalay>
2013-10-17 19:56:25 +0200

    Converting datamapper queries to raw SQL queries, half done, fixed some bugs because of this converion and improved layout.

--- helpers/games_helpers.rb
+++ helpers/games_helpers.rb
@@ -3,7 +3,7 @@
         @games = Game.find_by_sql("SELECT * FROM games ORDER BY title ASC")
       end
       def find_game
-        Game.get(params[:id].to_i)
+        Game.find_by_sql('SELECT id, title, released_on, description FROM games WHERE id =' + params[:id])
       end
       def create_game
         @game = Game.create(:title => params[:title], :description => params[:description], :released_on => params[:released_on], :submitted_on => Time.now)
@@ -14,3 +14,4 @@
     end
 
 #   find_by_sql("SELECT id, name FROM my_classes LIMIT 1", :properties => [:id, :name])
+
